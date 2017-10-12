e352d19ab9aae0b023fa23484403ccf1e9874d6e
KEYRUSCORP\Tarek.Chida <tarek.chida@keyrus.com>
2017-10-12 09:55:05 +0200

    upgrade :)

--- public/assets/main.js
+++ public/assets/main.js
@@ -2,16 +2,23 @@ var cv = {
     init: function () {
     },
     navigation: function () {
+        // Desktop menu
         $('.pt-trigger').click(function () {
             var pageNum = $(this).attr('data-goto');
 
             $('.pt-page').hide(50);
             $('.pt-page-' + pageNum).slideDown(500);
+            $('.site-nav').toggleClass('mobile-menu-hide');
 
+        });
+        // Mobile menu
+        $('.menu-toggle').click(function () {
+            $('.site-nav').toggleClass('mobile-menu-hide');
         });
     },
 };
 $(document).ready(function () {
     cv.init();
     cv.navigation();
+
 });
--- resources/views/portfolio.php
+++ resources/views/portfolio.php
@@ -9,18 +9,23 @@
         <div class="container portfolio-grid"> 
             <div class="row">
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Guar Tracker</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/guartracker.jpg" alt="Guar Tracker" style="width:100%">
                             <div class="overlay">
                                 <div class="text">
                                     <h5 class="name">Guar Tracker</h5>
+
+                                    <p>Lumen Framework, Jquery, Blockchain, REST API </p> 
+
                                 </div>
                             </div>
                         </a>
                     </div>
                 </figure>
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Ephemeride App</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/ephemeride.png" alt="Ephemeride App" style="width:100%">
@@ -33,6 +38,7 @@
                     </div>
                 </figure>
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Qe Pro</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/qepro.jpg" alt="Qe Pro" style="width:100%">
@@ -45,6 +51,7 @@
                     </div>
                 </figure>
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Beko</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/beko.jpg" alt="Beko" style="width:100%">
@@ -57,6 +64,7 @@
                     </div>
                 </figure>
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Lagardere Paris Racing</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/lpr.jpg" alt="Lagardere Paris Racing" style="width:100%">
@@ -69,18 +77,20 @@
                     </div>
                 </figure>
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Atelier Du Voyage</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/atlv.jpg" alt="Atelier Du Voyage" style="width:100%">
                             <div class="overlay">
                                 <div class="text">
-                                    <h5 class="name">Project Name</h5>
+                                    <h5 class="name">Atelier Du Voyage</h5>
                                 </div>
                             </div>
                         </a>
                     </div>
                 </figure>
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Accor Easypitch</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/easypitch.jpg" alt="Accor Easypitch" style="width:100%">
@@ -93,6 +103,7 @@
                     </div>
                 </figure>
                 <figure  class="col-md-3">
+                    <div class="d-md-none"> <h5 class="name">Equidia Club</h5></div>
                     <div class="thumbnail">
                         <a href="https:www.beko.fr" target="_blank">
                             <img src="/assets/images/portfolio/equidiaclub.jpg" alt="Equidia Club" style="width:100%">
